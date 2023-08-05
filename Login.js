cf7b6574cf0d92257714ed6b059afbe495d5bda1
akdevlabs <116324447+akdevlabs@users.noreply.github.com>
2023-08-04 22:52:32 -0600

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,62 @@
+const inputEl = document.getElementById("name")
+const inputP = document.getElementById("password")
+const subBtn = document.getElementById("submit-btn")
+const alertU = document.getElementById("alertU")
+const alertP = document.getElementById("alertP")
+
+subBtn.addEventListener("click", function() {
+userName()
+
+                                     })
+
+
+function userName(){
+let val = inputEl.value
+  if (val === "202301" ){
+    userPassword()
+  }else if (val ==="202302"){
+    userPassword()
+  }else if (val ==="202303"){
+    userPassword()
+  }else if (val ==="202304"){
+    userPassword()
+  }else if (val ==="202305"){
+    userPassword()
+  }else if (val ==="202306"){
+    userPassword()
+  }else {
+    alertU.innerHTML="Wrong Username or Password" 
+  }
+}
+
+function userPassword(){
+  let valp = inputP.value
+    console.log(valp)
+    if (valp === "6122023" ){
+      findUsername()
+    }else {
+      alertU.innerHTML="Wrong Username or Password"
+    }
+  }
+
+
+
+  function findUsername(){
+    let val = inputEl.value
+    if (val === "202301" ){
+      location.replace("https://joyful-lebkuchen-be5173.netlify.app/?greeting=202301")
+    }else if (val ==="202302"){
+      location.replace("https://joyful-lebkuchen-be5173.netlify.app/?greeting=202302")
+    }else if (val ==="202303"){
+      location.replace("https://joyful-lebkuchen-be5173.netlify.app/?greeting=202303")
+    }else if (val ==="202304"){
+      location.replace("https://joyful-lebkuchen-be5173.netlify.app/?greeting=202304")
+    }else if (val ==="202305"){
+      location.replace("https://joyful-lebkuchen-be5173.netlify.app/?greeting=202305")
+    }else if (val ==="202306"){
+      location.replace("https://joyful-lebkuchen-be5173.netlify.app/?greeting=202306")
+    }
+ }
+   
+
+
